667476858c0d56cffa952643788e69b2c9bfaa3e
tiweb <tiweb442@gmail.com>
2021-11-26 15:11:56 -0500

    console error fix

--- hh_vehextras/server.lua
+++ hh_vehextras/server.lua
@@ -62,9 +62,6 @@ RegisterCommand("givecar", function(source, args)
 	if plate == nil or plate == "" then plate = GeneratePlate() end
 	if veh ~= nil and args[1] ~= nil then
 		TriggerClientEvent('hhfw:client:givecar', args[1], veh, plate)
-	TriggerClientEvent("QBCore:Notify", source, "You gave vehilce to "..tPlayer.PlayerData.charinfo.firstname.." "..tPlayer.PlayerData.charinfo.lastname.." Vehicle :"..veh.." With Plate : "..plate, "success", 8000)
-	else 
-		TriggerClientEvent('QBCore:Notify', source, "Incorrect Format", "error")
 	end
 end, false)
 
