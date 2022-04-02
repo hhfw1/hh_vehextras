cac6fbe0c0a66f813a23ffebcb7305092ca3b366
hhfw1 <91369228+hhfw1@users.noreply.github.com>
2022-04-02 13:00:27 +0530

    Update for New QBCore

--- hh_vehextras/fxmanifest.lua
+++ hh_vehextras/fxmanifest.lua
@@ -1,12 +1,15 @@
 fx_version 'cerulean'
 game 'gta5'
 
+description 'hh_vehextras'
+version '2.0.0'
 
 client_scripts {
   'client.lua',
 }
 
 server_scripts {
+  '@oxmysql/lib/MySQL.lua',
   'server.lua',
 }
 
